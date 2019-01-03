cff2387aa19e7cf9cd7990fd46a16e14d325b6c7
roni46 <39713502+roni46@users.noreply.github.com>
2019-01-03 17:51:25 +0700

    Add files via upload

---
+++ tugas program periksa 2 bilangan terbesar atau terkecil.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{   int a,b;
+printf("PROGRAM PERIKSA 2 BILANGAN TERBESAR DAN TERKECIL\n");
+printf("BY M Achmad Sahroni NIM 311620782\n");
+printf("KAMPUS PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+cout<<"masukan nilai A :";
+cin>>a;
+cout<<"masukan nilai B :";
+cin>>b;
+if(a<b)
+{cout<<"B terbesar\n";
+
+}
+else if(a>b)
+{
+	cout<<"A terbesar\n";
+}
+else
+cout<<"error";
+}
